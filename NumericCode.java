1f80b8274aaba76cea1f1bb72772593a6f0f2967
Rahul Anand <rahul088anand@gmail.com>
2019-09-11 22:34:09 +0530

    new one

--- NumericCode.java
+++ NumericCode.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 class NumericCode{
 	public static void main(String args[]){
-		int i, j, X, P, M, lr, l, guess, XXX = 234, temp = 0, temp2 = 0, flag = 0;
+		int i, j, X, P, M, lr, l, guess, R = 234, temp = 0, temp2 = 0, flag = 0;
 		String out = "";
 		Scanner sc = new Scanner(System.in);
 		while(true){
-			temp = XXX;
+			temp = R;
 			guess = sc.nextInt();
 			for(i = 0; i < 3; i++){
 				lr = temp%10;
@@ -18,15 +18,16 @@ class NumericCode{
 						flag = 1;
 					else flag = 2;
 					temp2 = temp2/10;
-				}
 						if(flag == 0)
 							out = "M" + out;
 						else if(flag == 1)
 							out = "P" + out;
 						else out = "X" + out;
+				}
 				temp = temp/10;
 			}	
 				System.out.println(out);
+				out = "";
 				if(out.equals("MMM"))
 					break;
 		}

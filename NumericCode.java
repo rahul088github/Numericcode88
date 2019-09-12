c9105b5dcd3c43575835fedb975967be415dad60
Rahul Anand <rahul088anand@gmail.com>
2019-09-12 16:31:32 +0530

    check this

--- NumericCode.java
+++ NumericCode.java
@@ -10,26 +10,26 @@ class NumericCode{
 			for(i = 0; i < 3; i++){
 				lr = temp%10;
 				temp2 = guess;
-					for(j = 0; j < 3; j++){
-						l = temp2%10;
-						if(l==lr && i==j)
-							flag = 0;
-						else if(l==lr)
-							flag = 1;
-						else flag = 2;
-						temp2 = temp2/10;
+				for(j = 0; j < 3; j++){
+					l = temp2%10;
+					if(l==lr && i==j)
+						flag = 0;
+					else if(l==lr)
+						flag = 1;
+					else flag = 2;
+				temp2 = temp2/10;
 					}
-				temp = temp/10;
-						if(flag == 0)
-							out = "M" + out;
-						else if(flag == 1)
-							out = "P" + out;
-						else out = "X" + out;
+			temp = temp/10;
+			if(flag == 0)
+				out = "M" + out;
+			else if(flag == 1)
+				out = "P" + out;
+			else out = "X" + out;
 			}	
-				System.out.println(out);
-				out = "";
-				if(out.equals("MMM"))
-					break;
+			System.out.println(out);
+			out = "";
+			if(out.equals("MMM"))
+				break;
 		}
-}
+	}
 }	
